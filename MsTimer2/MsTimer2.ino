b8ae3f91b4460c42b2f46d5704832163110571e4
JunHyeon Mun <impelfin@gmail.com>
2017-12-06 00:36:18 +0900

    Update MsTimer2.ino

--- MsTimer2/MsTimer2.ino
+++ MsTimer2/MsTimer2.ino
@@ -16,7 +16,7 @@ boolean sw2state1 = false;
 boolean sw1state = false;
 boolean sw2state = false;
 
-void TimerExec(){
+void TimerExec() { 
    Count01++;       
    Count02++;       
    Count1++;      
@@ -24,35 +24,54 @@ void TimerExec(){
 }
 
 void setup() {
-      Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-            // opens serial port, sets data rate to 9600 bps
-        pinMode(LED1, OUTPUT);
-        pinMode(LED2, OUTPUT);
-        pinMode(SW1, INPUT_PULLUP);
-        pinMode(SW2, INPUT_PULLUP);
+   Serial.begin(9600);
+   pinMode(LED1, OUTPUT);
+   pinMode(LED2, OUTPUT);
+   pinMode(SW1, INPUT_PULLUP);
+   pinMode(SW2, INPUT_PULLUP);
         
-        MsTimer2::set(10,TimerExec);
-        MsTimer2::start();         
+   MsTimer2::set(10,TimerExec);
+   MsTimer2::start();         
 }  
 
-void loop() {     
-  
+void loop() {
    if(digitalRead(SW1) == LOW) {  
-     if(sw1state1 == true) { 
-       if(Count01 >= 1) { sw1state1 = false;
-          if(sw1state) { sw1state = false; digitalWrite(LED1, LOW); } 
-           else  { sw1state = true; Count1 = 0; }}
-     }  else  { sw1state1 = true; Count01 = 0; }
-   }
-   if(digitalRead(SW2) == LOW){  
-     if(sw2state1 == true) { 
-        if(Count02 >= 1) { sw2state1 = false;
-          if(sw2state) { sw2state = false;  digitalWrite(LED2, LOW); } 
-          else { sw2state = true; Count2 =0; }}
-     }  else  { sw2state1 = true; Count02 = 0; }
+      if(sw1state1 == true) { 
+          if(Count01 >= 1) {
+             sw1state1 = false;
+             if(sw1state) {
+                sw1state = false;
+                digitalWrite(LED1, LOW);
+             } else {
+                sw1state = true; 
+                Count1 = 0;
+             }
+          }
+      } else {
+         sw1state1 = true; 
+         Count01 = 0;
+      }
    }
    
-   if(sw1state == true) { 
+   if(digitalRead(SW2) == LOW) {
+      if(sw2state1 == true) {
+         if(Count02 >= 1) {
+            sw2state1 = false;
+            if(sw2state) {
+               sw2state = false;
+               digitalWrite(LED2, LOW);
+            } else {
+               sw2state = true; 
+               Count2 = 0;
+            }
+         }
+      } else {
+         sw2state1 = true; 
+         Count02 = 0;
+      }
+   }
+   
+   if(sw1state == true) 
       if(Count1 < 40)  digitalWrite(LED1, HIGH);
       if(Count1 > 40)  digitalWrite(LED1, LOW);
       if(Count1 > 80)  Count1 = 0;
