dc1e87e3052ca7e24911bca6be29794e51ab67ed
JunHyeon Mun <impelfin@gmail.com>
2018-06-11 20:15:52 +0900

    Update NeoPixel_Rainbow_Matrix.ino

--- NeoPixel_Rainbow_Matrix/NeoPixel_Rainbow_Matrix.ino
+++ NeoPixel_Rainbow_Matrix/NeoPixel_Rainbow_Matrix.ino
@@ -5,7 +5,12 @@
 #define COUNT 8
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-void display_PixelColor(int n, uint32_t c);
+
+void display_PixelColor(int led, uint32_t color) {
+      pixels.setPixelColor(led, color);
+      pixels.show(); 
+      delay(100); 
+}
 
 uint32_t C0 = 0x000000; //Black
 uint32_t C1 = 0xFFFFFF; //White
@@ -22,7 +27,7 @@ uint32_t C11 = 0x008080; //Teal
 
 uint32_t MyColors[12] = {C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11}; //put the colors in an array
 
-uint32_t c = MyColors[0]; 
+uint32_t color = MyColors[0]; 
 
 void setup() {
   pixels.begin();
@@ -30,16 +35,10 @@ void setup() {
 }
 
 void loop() {  
-  for(int i=0; i<NUMPIXELS; i++) {  
-    if(i % 2 == 0) c = MyColors[10]; else c = MyColors[11];
-    if((i % COUNT) == 6 || (i % COUNT) == 7) continue; else display_PixelColor(i, c);
+  for(int led=0; led<NUMPIXELS; led++) {  
+    if(led % 2 == 0) color = MyColors[10]; else color = MyColors[11];
+    if((led % COUNT) == 6 || (led % COUNT) == 7) continue; else display_PixelColor(led, color);
   }
   delay(1000);
   pixels.clear();
-}
-
-void display_PixelColor(int n, uint32_t c) {
-      pixels.setPixelColor(n, c);
-      pixels.show(); 
-      delay(100); 
 }
